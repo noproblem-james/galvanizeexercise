57d8d06b22befaf57386b7b07c689a40ab88c6eb
James Helfrich <james.helfrich@gmail.com>
2017-02-08 14:15:10 -0600

    I committed again!

---
+++ q2tables.sql
@@ -0,0 +1,32 @@
+CREATE TABLE Salesperson
+	(`ID` int, `Name` varchar(7), `Age` int, `Salary` int)
+;
+
+INSERT INTO Salesperson
+	(`ID`, `Name`, `Age`, `Salary`)
+VALUES
+	(1, 'Abe', 61, 140000),
+	(2, 'Bob', 34, 44000),
+	(5, 'Chris', 34, 40000),
+    (7, 'Dan', 41, 52000),
+    (8, 'Ken', 57, 115000),
+    (11, 'Joe', 38, 38000)
+
+;
+
+CREATE TABLE Orders
+	(`Number` int, `order_date` date, `cust_id` int, `salesperson_id` int, `Amount`, Int)
+;
+
+INSERT INTO Salesperson
+	(`Number`, `order_date`, `cust_id`, `salesperson_id`, `Amount`)
+VALUES
+	(10, 19960802, 4, 2, 540),
+	(20, 1990130, 4, 8, 1800),
+	(30, 19950714, 9, 1, 460),
+    (40, 19980129, 7, 2, 2400),
+    (50, 19980203, 6, 7, 600),
+    (60, 19980302, 6, 7, 720),
+    (70, 19980506, 9http://sqlfiddle.com/#, 7, 150_
+
+;
